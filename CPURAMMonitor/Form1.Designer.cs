4e1b9491c8e90af3cdc6b0e8e40cb435db6c2329
czaderdc <konrad0spoko@gmail.com>
2017-11-13 02:46:17 +0100

    jw

---
+++ CPURAMMonitor/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace CPURAMMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.metroProgressBar1 = new MetroFramework.Controls.MetroProgressBar();
+            this.daneCPULabel = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroProgressBar2 = new MetroFramework.Controls.MetroProgressBar();
+            this.daneRamLabel = new MetroFramework.Controls.MetroLabel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(36, 114);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(118, 19);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "Zużycie procesora:";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            // 
+            // metroProgressBar1
+            // 
+            this.metroProgressBar1.Location = new System.Drawing.Point(198, 114);
+            this.metroProgressBar1.Name = "metroProgressBar1";
+            this.metroProgressBar1.Size = new System.Drawing.Size(433, 23);
+            this.metroProgressBar1.TabIndex = 1;
+            // 
+            // daneCPULabel
+            // 
+            this.daneCPULabel.AutoSize = true;
+            this.daneCPULabel.Location = new System.Drawing.Point(650, 114);
+            this.daneCPULabel.Name = "daneCPULabel";
+            this.daneCPULabel.Size = new System.Drawing.Size(0, 0);
+            this.daneCPULabel.TabIndex = 2;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(36, 162);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(147, 19);
+            this.metroLabel2.TabIndex = 3;
+            this.metroLabel2.Text = "Dostępna pamięć RAM:";
+            // 
+            // metroProgressBar2
+            // 
+            this.metroProgressBar2.Location = new System.Drawing.Point(198, 162);
+            this.metroProgressBar2.Name = "metroProgressBar2";
+            this.metroProgressBar2.Size = new System.Drawing.Size(433, 23);
+            this.metroProgressBar2.TabIndex = 4;
+            // 
+            // daneRamLabel
+            // 
+            this.daneRamLabel.AutoSize = true;
+            this.daneRamLabel.Location = new System.Drawing.Point(650, 162);
+            this.daneRamLabel.Name = "daneRamLabel";
+            this.daneRamLabel.Size = new System.Drawing.Size(0, 0);
+            this.daneRamLabel.TabIndex = 5;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(20, 191);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "CPU";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "RAM";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(740, 345);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "wykres";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 556);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.daneRamLabel);
+            this.Controls.Add(this.metroProgressBar2);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.daneCPULabel);
+            this.Controls.Add(this.metroProgressBar1);
+            this.Controls.Add(this.metroLabel1);
+            this.Name = "Form1";
+            this.Text = "Monitor CPU i RAM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.Timer timer;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar1;
+        private MetroFramework.Controls.MetroLabel daneCPULabel;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar2;
+        private MetroFramework.Controls.MetroLabel daneRamLabel;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ CPURAMMonitor/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Management;
+
+namespace CPURAMMonitor
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        bool ostrzeglem = false;
+        PerformanceCounter cpuCounter;
+        PerformanceCounter ramCounter;
+        PerformanceCounter upTimeCounter;
+        SpeechSynthesizer synth;
+        PerformanceCounter uzywanyRAMCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cpuCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            upTimeCounter = new PerformanceCounter("System", "System Up Time");
+            upTimeCounter.NextValue();
+            timer.Tick += timer_Tick;
+            synth = new SpeechSynthesizer();
+            TimeSpan upTime = TimeSpan.FromSeconds(upTimeCounter.NextValue());
+            Task.Run(() =>
+            {
+                synth.Speak($"Your computer is up for {(int)upTime.TotalDays} days {(int)upTime.Hours} hours {(int)upTime.Minutes} minutes");
+            });
+
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            double daneCPU = cpuCounter.NextValue();
+            double daneRAM = ramCounter.NextValue();
+            
+            daneCPULabel.Text = string.Format("{0:0.00}%", daneCPU);
+            daneRamLabel.Text = daneRAM.ToString() + "MB";
+            this.metroProgressBar1.Value = (int)daneCPU;
+            this.metroProgressBar2.Value = (int)daneRAM/100;
+            chart1.Series["CPU"].Points.AddY(daneCPU);
+            chart1.Series["RAM"].Points.AddY(uzywanyRAMCounter.NextValue());//w gb
+            if ((int)daneRAM < 1000 && !ostrzeglem)
+            {
+                synth.Speak("Your free RAM is under one gigabyte!");
+                ostrzeglem = true;
+            }
+
+        }
+
+      
+    }
+}
